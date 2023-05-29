d113020aebfcd3d6e68800590d41934287ea0aec
RANA HASSAN <134930734+hassan42772@users.noreply.github.com>
2023-05-29 15:56:22 +0500

    Update list_slicing,tuple.py

--- list_slicing,tuple.py
+++ list_slicing,tuple.py
@@ -1,31 +1,31 @@
 
-# # list is mutable(value can be change)
+list is mutable(value can be change)
 
-# cricket_shop=["bat","ball","wicket","football","hockry","table-tennius"]
-# # append function
-# cricket_shop.append('basketball')
-# print(cricket_shop)
-# # remove function
-# cricket_shop.remove("wicket")
-# print(cricket_shop)
+cricket_shop=["bat","ball","wicket","football","hockry","table-tennius"]
+ # append function
+cricket_shop.append('basketball')
+ print(cricket_shop)
+ # remove function
+cricket_shop.remove("wicket")
+print(cricket_shop)
 # # insert function
-# cricket_shop.insert(3,"valle_ball")
-# print(cricket_shop)
-# # pop function
-# cricket_shop.pop()
-# print(cricket_shop)
-# # count function
-# cricket_shop.count("hockey")
-# print(cricket_shop)
-# # extend function
-# cricket_shop.extend(cricket_shop)
-# print(cricket_shop)
-# # index function
-# print(cricket_shop.index("ball"))
-# # claer function
-# # reverse function 
-# # tuple is unmutable (cannot change)
-# tp=(1,2,3,)
+cricket_shop.insert(3,"valle_ball")
+print(cricket_shop)
+# pop function
+cricket_shop.pop()
+print(cricket_shop)
+# count function
+cricket_shop.count("hockey")
+print(cricket_shop)
+ # extend function
+cricket_shop.extend(cricket_shop)
+print(cricket_shop)
+# index function
+print(cricket_shop.index("ball"))
+# claer function
+# reverse function 
+# tuple is unmutable (cannot change)
+tp=(1,2,3,)
 
 hassan_team=["ali","ahmad","bilal","haseeb"]
 # # sajjad_team=["anas","sufi","raza","imran"]
