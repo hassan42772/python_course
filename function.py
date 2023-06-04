6e1d10493693a24a932f22dcfaac3e960bf13d80
RANA HASSAN <134930734+hassan42772@users.noreply.github.com>
2023-06-04 23:30:51 +0500

    Add files via upload

---
+++ function.py
@@ -0,0 +1,12 @@
+def add(a,b):
+    sum=a+b
+    return sum
+print(add(2,4))
+
+def avr(a,b):
+    """this is average function of two numbers
+    """
+    average=(a+b)/2
+    return average
+aver=avr(12,12)
+print(avr.__doc__)
