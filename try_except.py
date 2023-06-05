4be792e8f792557f1e435c455ec99758bf278b3f
RANA HASSAN <134930734+hassan42772@users.noreply.github.com>
2023-06-05 15:05:51 +0500

    Add files via upload

---
+++ try_except.py
@@ -0,0 +1,9 @@
+print("please enter the number")
+num1=input()
+print("please enter the second number")
+num2=input()
+try:
+    print("sum of these number is", int(num1)+int(num2))
+except Exception as e:
+    print(e)
+print("this code is very important")
