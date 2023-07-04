2c58c648a9ac2747c417ecec853eb9defb3cac3b
RANA HASSAN <134930734+hassan42772@users.noreply.github.com>
2023-07-04 16:37:33 +0500

    Add files via upload

--- enumrate.py
+++ enumrate.py
@@ -1,4 +1,10 @@
-list1=["hassan","ahmad","bilal","ali"]
-for index , item in enumerate(list1):
-    if index % 2==0:
+# # enumarate function
+# list1=["hassan","ahmad","bilal","ali"]
+
+# for index , item in enumerate(list1):
+#     if index % 2==0:
+#         print(item)
+numbers=[1,2,3,4,5,6,7,8,9]
+for index , item in enumerate(numbers):
+    if index % 3==0:
         print(item)
