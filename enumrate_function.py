c3193800287bdc8a2fbec6467e49fdfd67b7884a
RANA HASSAN <134930734+hassan42772@users.noreply.github.com>
2023-07-04 16:38:24 +0500

    Update enumrate_function.py

--- enumrate_function.py
+++ enumrate_function.py
@@ -1,9 +1,9 @@
-# # enumarate function
-# list1=["hassan","ahmad","bilal","ali"]
+# enumarate function
+list1=["hassan","ahmad","bilal","ali"]
 
-# for index , item in enumerate(list1):
-#     if index % 2==0:
-#         print(item)
+for index , item in enumerate(list1):
+     if index % 2==0:
+        print(item)
 numbers=[1,2,3,4,5,6,7,8,9]
 for index , item in enumerate(numbers):
     if index % 3==0:
